8816db4a17a84b3f842367eadb2f12391408611a
Walid Zakaria <walidpiano@yahoo.com>
2018-04-18 12:39:20 +0200

    add image sending

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 
 folder_path = os.path.abspath(os.path.dirname(__file__))
+
+UPLOAD_FOLDER = 'uploaded_images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 #app.config["SQLALCHEMY_DATABASE_URI"] = f"""sqlite:///{os.path.join(folder_path, "my_database.db")}"""
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
