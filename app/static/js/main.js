9bf1a534dee0590cac6717129dc0f371fa0e47a3
walid zakaria adly <walidpiano@yahoo.com>
2018-04-29 08:39:24 +0200

    make try check browser

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -392,16 +392,24 @@ function inputOrSelection() {
 
 function browserOrApp() {
     result = false;
-    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-    var isIE = /*@cc_on!@*/false || !!document.documentMode;
-    var isEdge = !isIE && !!window.StyleMedia;
-    var isChrome = !!window.chrome && !!window.chrome.webstore;
-    var isBlink = (isChrome || isOpera) && !!window.CSS;
+    try {
+        var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+        var isFirefox = typeof InstallTrigger !== 'undefined';
+        var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+        var isIE = /*@cc_on!@*/false || !!document.documentMode;
+        var isEdge = !isIE && !!window.StyleMedia;
+        var isChrome = !!window.chrome && !!window.chrome.webstore;
+        var isBlink = (isChrome || isOpera) && !!window.CSS;
 
-    if (isOpera || isFirefox || isSafari || isIE || isEdge || isChrome || isBlink) {
-        result = true;
+        if (isOpera || isFirefox || isSafari || isIE || isEdge || isChrome || isBlink) {
+            result = true;
+        }
+
     }
-    return result;
+    catch(err) {}
+    finally {
+        return result;
+    }
+
+
 }
